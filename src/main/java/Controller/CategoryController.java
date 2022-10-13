ffcab24e41a09c741683a6331fe6653994a16df8
arodriguez <andres.rodriguez.mt@usa.edu.co>
2022-10-12 23:27:43 -0500

    El primer commit(aja el primero version 40000)

---
+++ src/main/java/Controller/CategoryController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Model.Category;
+import Service.CategoryService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author Andres Rodriguez
+ */
+@RestController
+@RequestMapping("/api/Category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/all")
+    public List<Category> getAll(){
+        return categoryService.getAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Optional<Category> getCategory(@PathVariable("id") Integer id){
+        return categoryService.getCategory(id);
+    }
+    
+    // /api/Category/save
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category save(@RequestBody Category category) {
+        return categoryService.save(category); }
+}   
